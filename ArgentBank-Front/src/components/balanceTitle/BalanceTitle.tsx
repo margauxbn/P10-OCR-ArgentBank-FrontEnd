ce6c36b68d101487d58e251df1bae48d8b7598ab
Margaux Burban <margaux@example.com>
2024-10-11 11:44:37 +0200

    Création de la page des transactions

--- ArgentBank-Front/src/components/balanceTitle/BalanceTitle.tsx
+++ ArgentBank-Front/src/components/balanceTitle/BalanceTitle.tsx
@@ -1,18 +1,18 @@
-import './balanceBlock.css';
+import './balanceTitle.css';
 import { Link } from 'react-router-dom';
 
-function BalanceBlock() {
+function BalanceTitle() {
   return (
     <>
-        <div className='balance-block'>
+        <div className='balance-title-block'>
             <div className='balance-information'>
                 <h3 className='balance-title'>Argent Bank Checking x8349</h3>
                 <p className='balance-amount'>$2,082.79</p>
                 <p className='balance-information'>Available Balance</p>    
             </div>
             <div>
-                <Link to ='/transaction'>
-                    <button className='btn-view-transaction'>View transactions</button>
+                <Link to ='/user'>
+                    <button className='btn-cross'>X</button>
                 </Link>
             </div>        
         </div>
@@ -20,4 +20,4 @@ function BalanceBlock() {
   )
 }
 
-export default BalanceBlock
+export default BalanceTitle
