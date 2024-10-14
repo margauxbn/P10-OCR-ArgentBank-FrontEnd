fbd5e5850183322837c3d3f528ba539d7b51d7bc
Margaux Burban <margaux@example.com>
2024-10-14 11:17:51 +0200

    Création du fichier .json et remplacement des éléments par les props

--- ArgentBank-Front/src/App.tsx
+++ ArgentBank-Front/src/App.tsx
@@ -1,19 +1,24 @@
 import './App.css';
 import Header from "./components/header/Header";
 import Feature from './components/feature/Feature';
+import data from "./database.json";
 
 function App() {
   return (
     <>
-      <Header />
+       <Header />
       <div className='features-section'>
-        <Feature />
-        <Feature />
-        <Feature />        
+        {data.features.map((feature, index) => (
+          <Feature 
+            key={index} 
+            icon={feature.icon} 
+            title={feature.title} 
+            description={feature.description} 
+          />
+        ))}
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
